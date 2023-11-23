96e269945e9554e028ef172653c7b71a9b11e7fe
Know167 <191200116035@asoit.edu.in>
2023-11-23 03:44:48 -0500

    Saurav UI components added
    Product Details page and Update Product Details

--- eco-threads-frontend/src/App.js
+++ eco-threads-frontend/src/App.js
@@ -2,7 +2,11 @@ import { Box } from "@mui/material";
 import "./App.css";
 import MenuBar from "./components/Layout/MenuBar";
 import Home from "./pages/Home";
-
+import { ProductDetails } from './pages/product/ProductDetails';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { UpdateProductDetails } from './pages/product/updateproductdetails/UpdateProductDetails';
 function App() {
     return (
         <Box component="main" sx={{ p: 3 }}>
