f1a3125721903b4278e614869897305d292e1aa1
sauravpatel186 <59532863+sauravpatel186@users.noreply.github.com>
2023-11-23 05:25:47 -0500

    app js modified

--- eco-threads-frontend/src/App.js
+++ eco-threads-frontend/src/App.js
@@ -2,13 +2,21 @@ import { Box } from "@mui/material";
 import "./App.css";
 import MenuBar from "./components/Layout/MenuBar";
 import Home from "./pages/Home";
-
+import { ProductDetails } from './pages/product/ProductDetails';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { UpdateProductDetails } from './pages/product/updateproductdetails/UpdateProductDetails';
+import { Login } from './pages/login/Login';
 function App() {
     return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
         <Box component="main" sx={{ p: 3 }}>
             <MenuBar />
             <Home />
         </Box>
+    </LocalizationProvider>
+
     );
 }
 
